e16e3a636c232afc6f6c59c2e5abb43cb8903923
mateyo3 <mateoreyes720@gmail.com>
2018-01-13 12:33:26 -0800

    bootstrap template added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
+var mongojs = require("mongojs");
+
 
 // Initialize Express
 var app = express();
@@ -23,7 +25,7 @@ db.on("error", function(error) {
 
 // Main route (simple Hello World Message)
 app.get("/", function(req, res) {
-  res.send("Hello world");
+  res.send("LA Times Scraper");
 });
 
 // Retrieve data from the db
@@ -79,43 +81,43 @@ app.get("/scrape", function(req, res) {
 });
 
 
-// First, tell the console what server.js is doing
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from LA Times webdev board:" +
-            "\n***********************************\n");
+// // First, tell the console what server.js is doing
+// console.log("\n***********************************\n" +
+//             "Grabbing every thread name and link\n" +
+//             "from LA Times webdev board:" +
+//             "\n***********************************\n");
 
-// Making a request for the LA Times "webdev" board. The page's HTML is passed as the callback's third argument
-request("http://www.latimes.com/", function(error, response, html) {
+// // Making a request for the LA Times "webdev" board. The page's HTML is passed as the callback's third argument
+// request("http://www.latimes.com/", function(error, response, html) {
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
+//   // Load the HTML into cheerio and save it to a variable
+//   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+//   var $ = cheerio.load(html);
 
-  // An empty array to save the data that we'll scrape
-  var results = [];
+//   // An empty array to save the data that we'll scrape
+//   var results = [];
 
-  // With cheerio, find each h3-tag with the "trb_outfit_relatedListTitle" class
-  // (i: iterator. element: the current element)
-  $("h3.trb_outfit_relatedListTitle").each(function(i, element) {
+//   // With cheerio, find each h3-tag with the "trb_outfit_relatedListTitle" class
+//   // (i: iterator. element: the current element)
+//   $("h3.trb_outfit_relatedListTitle").each(function(i, element) {
 
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
+//     // Save the text of the element in a "title" variable
+//     var title = $(element).text();
 
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
+//     // In the currently selected element, look at its child elements (i.e., its a-tags),
+//     // then save the values for any "href" attributes that the child elements may have
+//     var link = $(element).children().attr("href");
 
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
-    });
-  });
+//     // Save these results in an object that we'll push into the results array we defined earlier
+//     results.push({
+//       title: title,
+//       link: link
+//     });
+//   });
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
+//   // Log the results once you've looped through each of the elements found with cheerio
+//   console.log(results);
+// });
 
 // Listen on port 3000
 app.listen(5000, function() {
