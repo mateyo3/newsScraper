a5794022c1d9baf3df7779216296097283c39928
mateyo3 <mateoreyes720@gmail.com>
2018-01-12 17:41:48 -0800

    initial setup and require all dependencies

---
+++ server.js
@@ -0,0 +1,6 @@
+var cheerio = require("cheerio");
+var request = require("request");
+var bodyParser = require("body-parser");
+var express = require("express");
+var handlebars = require("express-handlebars");
+var mongoose = require("mongoose");
