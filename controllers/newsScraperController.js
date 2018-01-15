47601f9b76fb87e0618be77d98eb5a6072dfb670
mateyo3 <mateoreyes720@gmail.com>
2018-01-14 23:48:19 -0800

    save

--- controllers/newsScraperController.js
+++ controllers/newsScraperController.js
@@ -14,31 +14,26 @@ router.get("/", function(req, res) {
 
 // Database configuration
 var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+var collections = ["scraperData"];
 
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
+
+// mongojs errors
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-// Retrieve data from the db
-router.get("/all", function(req, res) {
-  // Find all results from the scrapedData collection in the db
-  db.scrapedData.find({}, function(error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
-  });
+
+//ROUTES
+
+router.get("/", function(req, res) {
+  res.send(index.handlebars);
 });
 
+
 // Scrape data from one site and place it into the mongodb db
-router.get("/console", function(req, res) {
+router.get("/home", function(req, res) {
   // Make a request for the news section of ycombinator
   request("http://www.latimes.com/", function(error, response, html) {
     // Load the html body from request into cheerio
@@ -52,7 +47,7 @@ router.get("/console", function(req, res) {
       // If this found element had both a title and a link
       if (title && link) {
         // Insert the data in the scrapedData db
-        db.scrapedData.insert({
+        db.scraperData.insert({
           title: title,
           link: link
         },
@@ -63,55 +58,76 @@ router.get("/console", function(req, res) {
           }
           else {
             // Otherwise, log the inserted data
-            console.log(inserted);
+            console.log("DATA HAS BEEN INSERTED INTO MONGO DATABASE\n" + 
+                        "*****************************\n\n" + inserted);
+            // res.send(inserted);
           }
         });
       }
     });
   });
 
-  // Send a "Scrape Complete" message to the browser
-  res.render("index", db.scrapedData);
-});
-
-
-// First, tell the console what server.js is doing
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from LA Times webdev board:" +
-            "\n***********************************\n");
-
-// Making a request for the LA Times "webdev" board. The page's HTML is passed as the callback's third argument
-request("http://www.latimes.com/", function(error, response, html) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // With cheerio, find each h3-tag with the "trb_outfit_relatedListTitle" class
-  // (i: iterator. element: the current element)
-  $("h3.trb_outfit_relatedListTitle").each(function(i, element) {
-
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
-
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
-    });
+  // Retrieve data from the db
+router.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scraperData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+      console.log("route /all was successful in finding collection");
+    }
   });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
 });
+
+
+  // Send a "Scrape Complete" message to the browser
+  // res.send("index", inserted);
+  console.log("LA Times has been scraped");
+
+});
+
+
+// // First, tell the console what server.js is doing
+// console.log("\n***********************************\n" +
+//             "Grabbing every thread name and link\n" +
+//             "from LA Times webdev board:" +
+//             "\n***********************************\n");
+
+// // Making a request for the LA Times "webdev" board. The page's HTML is passed as the callback's third argument
+// request("http://www.latimes.com/", function(error, response, html) {
+
+//   // Load the HTML into cheerio and save it to a variable
+//   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+//   var $ = cheerio.load(html);
+
+//   // An empty array to save the data that we'll scrape
+//   var results = [];
+
+//   // With cheerio, find each h3-tag with the "trb_outfit_relatedListTitle" class
+//   // (i: iterator. element: the current element)
+//   $("h3.trb_outfit_relatedListTitle").each(function(i, element) {
+
+//     // Save the text of the element in a "title" variable
+//     var title = $(element).text();
+
+//     // In the currently selected element, look at its child elements (i.e., its a-tags),
+//     // then save the values for any "href" attributes that the child elements may have
+//     var link = $(element).children().attr("href");
+
+//     // Save these results in an object that we'll push into the results array we defined earlier
+//     results.push({
+//       title: title,
+//       link: link
+//     });
+//   });
+
+//   // Log the results once you've looped through each of the elements found with cheerio
+//   console.log("Data has been scraped");
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
