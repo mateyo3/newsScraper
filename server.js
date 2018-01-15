5383cfb2ce816b73c17e13e93b7c2eb9bd5e3c9d
mateyo3 <mateoreyes720@gmail.com>
2018-01-14 22:23:38 -0800

    moved scraper functions to controller/

--- server.js
+++ server.js
@@ -4,84 +4,31 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
-var mongojs = require("mongojs");
 
 
 // Initialize Express
+var port = process.env.PORT || 5000;
+
 var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+// Import routes and give the server access to them.
+var routes = require("./controllers/newsScraperController.js");
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+app.use("/", routes);
 
 
-
-
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.render("index");
-});
-
-// Retrieve data from the db
-app.get("/all", function(req, res) {
-  // Find all results from the scrapedData collection in the db
-  db.scrapedData.find({}, function(error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
-  });
-});
-
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-  // Make a request for the news section of ycombinator
-  request("http://www.latimes.com/", function(error, response, html) {
-    // Load the html body from request into cheerio
-    var $ = cheerio.load(html);
-    // For each element with a "title" class
-    $("h3.trb_outfit_relatedListTitle").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var title = $(element).children("a").text();
-      var link = $(element).children("a").attr("href");
-
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
-    });
-  });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
+//   // Send a "Scrape Complete" message to the browser
+//   res.send("Scrape Complete");
+// });
 
 
 // // First, tell the console what server.js is doing
